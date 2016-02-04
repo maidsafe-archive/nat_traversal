3a418c0c04262708b30f772ba05213031d748e00
MaidSafe-QA <qa@maidsafe.net>
2016-02-04 13:36:59 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.TwoWayStrategy.js
+++ master/implementors/core/str/pattern/trait.TwoWayStrategy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['bitflags'] = [];implementors['socket_addr'] = [];implementors['hyper'] = [];implementors['igd'] = [];
+implementors['regex_syntax'] = [];implementors['bitflags'] = [];implementors['libc'] = [];implementors['socket_addr'] = [];implementors['hyper'] = [];implementors['igd'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
