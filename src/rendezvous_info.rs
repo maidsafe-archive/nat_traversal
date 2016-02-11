96d76b19de8dce2610122bb00eee05c64d0d98e6
Andrew Cann <shum@canndrew.org>
2016-02-11 19:53:41 +0800

    derive(Debug) for rendezvous info structs

--- src/rendezvous_info.rs
+++ src/rendezvous_info.rs
@@ -23,7 +23,7 @@ use rand;
 use mapped_socket_addr::MappedSocketAddr;
 
 /// Info exchanged by both parties before performing a rendezvous connection.
-#[derive(Clone, PartialEq, Eq, RustcEncodable, RustcDecodable)]
+#[derive(Debug, Clone, PartialEq, Eq, RustcEncodable, RustcDecodable)]
 pub struct PubRendezvousInfo {
     /// A vector of all the mapped addresses that the peer can try connecting to.
     endpoints: Vec<MappedSocketAddr>,
@@ -32,7 +32,7 @@ pub struct PubRendezvousInfo {
 }
 
 /// The local half of a `PubRendezvousInfo`.
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PrivRendezvousInfo {
     secret: [u8; 4],
 }
