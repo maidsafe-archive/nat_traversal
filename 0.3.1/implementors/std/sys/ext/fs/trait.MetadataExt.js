11dd2252174d2481a6938ca99f6f3d09140da9cf
MaidSafe-QA <qa@maidsafe.net>
2016-03-16 09:07:05 +0000

    Updated documentation.

---
+++ 0.3.1/implementors/std/sys/ext/fs/trait.MetadataExt.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['socket_addr'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
